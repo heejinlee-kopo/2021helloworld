3daff2720af323ac9eefc63b8e8e9c7e4c932bc7
heejinlee <heejinlee@LAPTOP-RMM0FDLH>
2021-02-14 22:29:47 +0900

    master first commit 10:29.p.m.

---
+++ src/git/kopo/ac/kr/HelloWorld.java
@@ -0,0 +1,10 @@
+package git.kopo.ac.kr;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+
+}
